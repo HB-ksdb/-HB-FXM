98ef04cd214c07982667e757a6f84e630e025423
越 HD脚本 <yanglizhiyanglizhi000@gmail.com>
2025-10-19 20:19:56 +0800

    Add files via upload

---
+++ 4.HB FXM 检测服务器.lua
@@ -0,0 +1,76 @@
+-- ================ 检测服务器 ================
+-- ID服务器号
+-- =  =
+if game.PlaceId == 6839171747 then--HB FXM doors
+    YIJIAOBEN = "YIJIAOBEN"
+loadstring(game:HttpGet("https://raw.githubusercontent.com/HB-ksdb/-HB-FXM/main/1.HB%20FXM%20DOORS%E5%8A%A0%E5%AF%86.txt",true))()
+-- =  =
+-- ================  ================
+-- =  =
+elseif game.PlaceId == 189707 then--自然灾害 
+    YIJIAOBEN = "YIJIAOBEN"
+loadstring(game:HttpGet("https://raw.githubusercontent.com/HB-ksdb/-4/main/%E8%B6%8A%20HB%E8%87%AA%E7%84%B6%E7%81%BE%E5%AE%B3%E9%80%89%E6%8B%A9.lua",true))()
+-- =  =
+-- ================  ================
+-- =  =
+elseif game.PlaceId == 5256165620 then--火箭发射模拟器 
+    YIJIAOBEN = "YIJIAOBEN"
+loadstring(game:HttpGet("https://raw.githubusercontent.com/HB-ksdb/-4/main/%E8%B6%8A%20HB%20%E7%81%AB%E7%AE%AD%E5%8F%91%E5%B0%84%E6%A8%A1%E6%8B%9F%E5%99%A8%E9%80%89%E6%8B%A9.lua",true))()
+-- =  =
+-- ================  ================
+-- =  =
+elseif game.PlaceId == 4639625707 then-- 战争大享
+    YIJIAOBEN = "YIJIAOBEN"
+loadstring(game:HttpGet("https://raw.githubusercontent.com/HB-ksdb/-HB-FXM/main/2.HB%20FXM%20%E6%88%98%E4%BA%89%E5%A4%A7%E4%BA%AB%E5%8A%A0%E5%AF%86.txt",true))()
+-- =  =
+-- ================  ================
+-- =  =
+elseif game.PlaceId == 3956818381 then-- 忍者传奇收费
+    YIJIAOBEN = "YIJIAOBEN"
+loadstring(game:HttpGet("https://raw.githubusercontent.com/HB-ksdb/-4/main/%E5%BF%8D%E8%80%85%E4%BC%A0%E5%A5%87.txt",true))()
+-- =  =
+-- ================  ================
+-- =  =
+elseif game.PlaceId == 3623096087 then-- 力量传奇
+    YIJIAOBEN = "YIJIAOBEN"
+loadstring(game:HttpGet("https://raw.githubusercontent.com/HB-ksdb/-4/main/%E5%8A%9B%E9%87%8F%E4%BC%A0%E5%A5%87.txt",true))()
+-- =  =
+-- ================  ================
+-- =  =
+elseif game.PlaceId == 126509999114328 then-- 99夜
+    YIJIAOBEN = "YIJIAOBEN"
+loadstring(game:HttpGet("https://raw.githubusercontent.com/HB-ksdb/-4/main/%E8%B6%8AHB%20FXM%2099%E5%A4%9C%E9%80%89%E6%8B%A9.lua",true))()
+-- =  =
+-- ================  ================
+-- =  =
+elseif game.PlaceId == 3101667897 then-- 极速传奇
+    YIJIAOBEN = "YIJIAOBEN"
+loadstring(game:HttpGet("https://raw.githubusercontent.com/HB-ksdb/-4/main/%E6%9E%81%E9%80%9F%E4%BC%A0%E5%A5%87.txt",true))()
+-- =  =
+-- ================  ================
+-- =  =
+elseif game.PlaceId == 137953123589281 then-- 环山军区
+    YIJIAOBEN = "YIJIAOBEN"
+loadstring(game:HttpGet("https://raw.githubusercontent.com/HB-ksdb/-HB-FXM/main/3.HB%20FXM%20%E7%8E%AF%E5%B1%B1%E5%86%9B%E5%8C%BA%E5%8A%A0%E5%AF%86.txt",true))()
+-- =  =
+-- ================  ================
+-- =  =
+elseif game.PlaceId == 10449761463 then-- 最强战场
+    YIJIAOBEN = "YIJIAOBEN"
+loadstring(game:HttpGet("https://raw.githubusercontent.com/HB-ksdb/-4/main/%E8%B6%8A%20HBFXM%E6%9C%80%E5%BC%BA%E6%88%98%E5%9C%BA%E9%80%89%E6%8B%A9.lua",true))()
+-- =  =
+-- ================  ================
+-- =  =
+elseif game.PlaceId == 13772394625 then-- 刀刃球
+    YIJIAOBEN = "YIJIAOBEN"
+loadstring(game:HttpGet("https://raw.githubusercontent.com/HB-ksdb/-4/main/%E8%B6%8AHB%20FXM%E5%88%80%E5%88%83%E7%90%83%E9%80%89%E6%8B%A9.lua",true))()
+-- =  =
+-- ================  ================
+-- =  =
+else--通用
+    YIJIAOBEN = "YIJIAOBEN"
+loadstring(game:HttpGet("https://raw.githubusercontent.com/HB-ksdb/-4/main/%E4%B8%BB%E8%84%9A%E6%9C%AC.txt",true))()
+end
+-- =  =
+-- ================ 顶 ================
+-- 
